4357732548ee6765d178f0eca3d456ade2ae6e3f
Paul Stadig <paul@stadig.name>
2017-10-04 05:50:57 -0400

    Version 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pjstadig/assert-diff "0.1.0"
+(defproject pjstadig/assert-diff "0.2.0-SNAPSHOT"
   :description "Fine grained equality assertions for Clojure data structures."
   :url "https://github.com/pjstadig/assert-diff"
   :license {:name "Eclipse Public License"
